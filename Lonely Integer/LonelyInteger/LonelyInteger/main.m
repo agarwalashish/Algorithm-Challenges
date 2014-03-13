320a2669f9856a5bcf835502321a68bb81420fb1
agarwalashish <ashish-agarwal@hotmail.com>
2014-03-12 19:55:23 -0700

    Initial Commit

---
+++ Lonely Integer/LonelyInteger/LonelyInteger/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LonelyInteger
+//
+//  Created by Ashish Agarwal on 2014-03-12.
+//  Copyright (c) 2014 Ashish Agarwal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
